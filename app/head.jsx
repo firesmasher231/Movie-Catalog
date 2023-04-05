b3f533508fadcbaa9ee222c1ed5f5eebf857a233
firesmasher231 <66473811+firesmasher231@users.noreply.github.com>
2023-04-05 02:19:40 +0100

    Initial Commit

---
+++ app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+	return (
+		<>
+			<title>MovieLog</title>
+			<meta name="description" content="Created by AJ" />
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<link rel="icon" href="/favicon.ico" />
+		</>
+	);
+}
